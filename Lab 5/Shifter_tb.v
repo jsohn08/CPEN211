4e4c6b463a1769e27f6322b8a446d42818bb94d1
Muchen He <mansurhe@outlook.com>
2016-10-15 12:42:54 -0700

    added and tested shifter

--- Lab 5/Shifter_tb.v
+++ Lab 5/Shifter_tb.v
@@ -5,4 +5,26 @@ module Shifter_tb();
 
   Shifter #(8) DUT(sim_in, sim_out, sim_shift);
 
-  
+  initial begin
+    sim_shift = 2'b00;
+    sim_in = 8'b00001111;
+    #5;
+
+    // shift left
+    sim_shift = 2'b01;
+    #5;
+
+    // shift right with 0 being MSB
+    sim_shift = 2'b10;
+    #5;
+
+    // shift right with in[15] being MSB
+    sim_in = 8'b10000000;
+    sim_shift = 2'b11;
+    #5;
+
+    sim_in = 8'b01000000;
+    sim_shift = 2'b11;
+    #5;
+  end
+endmodule
