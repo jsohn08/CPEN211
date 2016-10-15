6c9f23851c1ce100a999d265ebdf24e792a46e58
Muchen He <mansurhe@outlook.com>
2016-10-14 22:07:53 -0700

    added test bench for MUX8

--- Lab 5/regfile.v
+++ Lab 5/regfile.v
@@ -140,3 +140,32 @@ module Mux2_tb();
     #5; select = 0;
   end
 endmodule
+
+module Mux8_tb();
+  reg [2:0] sim_a0, sim_a1, sim_a2, sim_a3, sim_a4, sim_a5, sim_a6, sim_a7;
+  reg [7:0] sim_select;
+  wire [2:0] sim_b;
+
+  // device under test
+  Mux8 #(3) DUT(sim_a0, sim_a1, sim_a2, sim_a3, sim_a4, sim_a5, sim_a6, sim_a7,
+    sim_select, sim_b);
+
+  // test
+  initial begin
+    // initial state
+    sim_a0 = 3'd0;
+    sim_a1 = 3'd1;
+    sim_a2 = 3'd2;
+    sim_a3 = 3'd3;
+    sim_a4 = 3'd4;
+    sim_a5 = 3'd5;
+    sim_a6 = 3'd6;
+    sim_a7 = 3'd7;
+    sim_select = 8'b0000_0001;
+
+    // select test
+    repeat(8) begin
+      #5; sim_select = sim_select << 1;
+    end
+  end
+endmodule
