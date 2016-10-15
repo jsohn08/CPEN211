dd4f9ab3bb37c91077158e3595d199dec76dc6bd
Muchen He <mansurhe@outlook.com>
2016-10-15 15:20:30 -0700

    finished datapath module

--- Lab 5/datapath.v
+++ Lab 5/datapath.v
@@ -53,15 +53,27 @@ module datapath (
 
   // wires
   wire [15:0] data_in, data_out,
-              loada_out, loadb_out,
+              RA_out, RB_out,
               shifter_out,
               A_in, B_in,
               ALU_out, ALU_status,
-              loadc_out;
+              rc_out;
 
   // modules
   MUX2 #(16) M0(loadc_out, datapath_in, vsel, data_in);
 
   regfile #(16) RF(data_in, data_out, readnum, writenum, write, clk);
 
-  
+  register #(16) RA(data_out, RA_out, loada, clk);
+  register #(16) RB(data_out, RB_out, loadb, clk);
+
+  shifter #(16) S0(RB_out, shifter_out, shift);
+
+  MUX2 #(16) MA(RA_out, 16'b0, asel, A_in);
+  MUX2 #(16) MA(shifter_out, {11'b0, datapath_in[4:0]}, bsel, B_in);
+
+  alu #(16) ALU(A_in, B_in, ALUop, ALU_out, ALU_status);
+
+  register #(16) RC(ALU_out, datapath_out, loadc, clk);
+  register #(1) RS(ALU_status, status, loads, clk);
+endmodule
