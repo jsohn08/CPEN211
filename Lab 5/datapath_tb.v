f4c59d5c087cdcda0523ac8d2a8e3dee42b08e36
Muchen He <mansurhe@outlook.com>
2016-10-16 11:22:46 -0700

    finished writing datapath_tb

--- Lab 5/datapath_tb.v
+++ Lab 5/datapath_tb.v
@@ -1,4 +1,4 @@
-module datapath_tb()
+module datapath_tb();
 
   // clock
   reg clk;
@@ -32,9 +32,53 @@ module datapath_tb()
   end
 
   initial begin
+    #1;
+
     // store 7 in R0
+    write = 1;
+    vsel = 0;
     datapath_in = 16'd7;
     writenum = 3'd0;
+    #10;
+
+    // store 2 in R1
+    datapath_in = 16'd2;
+    writenum = 3'd1;
+    #10;
+    write = 0;
+
+    // fetch R0 to RB (for shifting)
+    readnum = 3'd0;
+    loada = 0;
+    loadb = 1;
+    #10;
+    loadb = 0;
+
+    // fetch R1 to RA
+    readnum = 3'd1;
+    loada = 1;
+    loadb = 0;
+    #10;
+    loada = 0;
+
+    // shift RB by 1 and add
+    shift = 2'd01;
+    ALUop = 2'd0;
+    asel = 0;
+    bsel = 0;
+    loadc = 1;
+    loads = 1;
+    #10;
+    loadc = 0;
+    loads = 0;
+
+    // store RC to R2
     write = 1;
-    
+    vsel = 1;
+    writenum = 3'd2;
+    #10;
+
+    // stop sim
+    $stop;
   end
+endmodule
