43b39de02121e3ec359874459bfb5dac191b9363
Muchen He <mansurhe@outlook.com>
2016-10-23 22:07:43 -0700

    completed stage 1, im coming back to that figure 8 test bench later

--- Lab 6/datapath_tb.v
+++ Lab 6/datapath_tb.v
@@ -96,67 +96,7 @@ module datapath_tb();
     // clock cycle i+2
     #10;
 
-    // stop sim
-    $stop;
-  end
-endmodule
-
-module datapath_tb2();
-
-  // clock
-  reg clk;
-
-  // fetch
-  reg [2:0] readnum;
-  reg vsel, loada, loadb;
-
-  // excute
-  reg [1:0] shift, ALUop;
-  reg asel, bsel, loadc, loads;
-
-  // writeback
-  reg [15:0] datapath_in;
-  reg [2:0] writenum;
-  reg write;
-
-  // stage 1
-  reg loadpc, loadir, reset, mwrite, msel;
-
-  // output
-  wire [15:0] datapath_out;
-  wire status;
-
-  // datapath module
-  datapath DUT( clk,
-                readnum,
-                vsel,
-                loada,
-                loadb,
-                shift,
-                asel,
-                bsel,
-                ALUop,
-                loadc,
-                loads,
-                writenum,
-                write,
-                datapath_in,
-                status,
-                datapath_out,
-
-                // lab 6 stage 1
-                loadpc, loadir, reset, mwrite, msel);
-
-  initial begin
-    forever begin
-      clk = 0; #5;
-      clk = 1; #5;
-    end
-  end
-
-  initial begin
     // testing writing to memory
-
     // clock cycle i
     msel = 1;
     mwrite = 1;
