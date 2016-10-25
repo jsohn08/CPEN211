f0e7ffa4299c5f1bc41914ecbd70e72809283b88
Muchen He <mansurhe@outlook.com>
2016-10-24 23:29:06 -0700

    added switches that physically connect to de1 in top module

--- Lab 6/lab6_top.v
+++ Lab 6/lab6_top.v
@@ -1,7 +1,11 @@
-module lab6_top(reset, clk);
-  // TODO: wire these as switches
-  //input reset, clk;
-  input reset, clk;
+module lab6_top(KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3);
+  input [3:0] KEY;
+  input [9:0] SW;
+  output [9:0] LEDR;
+  output [6:0] HEX0, HEX1, HEX2, HEX3;
+
+  wire reset = KEY[1];
+  wire clk = KEY[0];
 
   wire [15:0] instructions_in, sximm5, sximm8;
   wire [2:0] opcode, readnum, writenum;
@@ -28,4 +32,57 @@ module lab6_top(reset, clk);
     loadir, loadpc, msel, mwrite, nsel,
     vsel, write, asel, bsel, loada, loadb, loadc, loads);
 
+  // seven segment modules (from lab5)
+  sseg H0(datapath_out[3:0],   HEX0);
+  sseg H1(datapath_out[7:4],   HEX1);
+  sseg H2(datapath_out[11:8],  HEX2);
+  sseg H3(datapath_out[15:12], HEX3);
+endmodule
+
+// seven segment modules (from lab5)
+module sseg(in,segs);
+  input [3:0] in;
+  output reg [6:0] segs;
+
+  // easier for the seven segment things
+  // numbers
+  `define ZERO  7'b1000000
+  `define ONE   7'b1111001
+  `define TWO   7'b0100100
+  `define THREE 7'b0110000
+  `define FOUR  7'b0011001
+  `define FIVE  7'b0010010
+  `define SIX   7'b0000010
+  `define SEVEN 7'b1111000
+  `define EIGHT 7'b0000000
+  `define NINE  7'b0010000
+
+  // letters
+  `define A     7'b0001000
+  `define B     7'b0000011
+  `define C     7'b1000110
+  `define D     7'b0100001
+  `define E     7'b0000110
+  `define F     7'b0001110
+
+  always @(*) begin
+    case (in)
+      4'b0000: segs = `ZERO;
+      4'b0001: segs = `ONE;
+      4'b0010: segs = `TWO;
+      4'b0011: segs = `THREE;
+      4'b0100: segs = `FOUR;
+      4'b0101: segs = `FIVE;
+      4'b0110: segs = `SIX;
+      4'b0111: segs = `SEVEN;
+      4'b1000: segs = `EIGHT;
+      4'b1001: segs = `NINE;
+      4'b1010: segs = `A;
+      4'b1011: segs = `B;
+      4'b1100: segs = `C;
+      4'b1101: segs = `D;
+      4'b1110: segs = `E;
+      4'b1111: segs = `F;
+    endcase
+  end
 endmodule
--- Lab 6/lab6_top_tb.v
+++ Lab 6/lab6_top_tb.v
@@ -1,18 +1,21 @@
 module lab6_top_tb();
-  reg reset, clk;
+  reg [3:0] KEY;
+  reg [9:0] SW;
+  wire [9:0] LEDR;
+  wire [3:0] HEX0, HEX1, HEX2, HEX3;
 
-  lab6_top DUT(reset, clk);
+  lab6_top DUT(KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3);
 
   initial begin
     repeat (20) begin
-      clk = 1; #5;
-      clk = 0; #5;
+      KEY[0] = 1; #5;
+      KEY[0] = 0; #5;
     end
   end
 
   initial begin
-    reset = 1;
+    KEY[1] = 1;
     #6;
-    reset = 0;
+    KEY[1] = 0;
   end
 endmodule
