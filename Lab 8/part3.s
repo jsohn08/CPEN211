b43887356ac60a15c0a5b143812f25ed1f786b41
Muchen He <mansurhe@outlook.com>
2016-11-06 14:08:16 -0800

    added part3.s - need to convertfiles directory to one with .s extension at the end

---
+++ Lab 8/part3.s
@@ -0,0 +1,12 @@
+// before program
+        .include "address_map_arm"
+        .text
+        .global _start
+
+// setup variables for counting
+_start:
+        LDR R0, =LEDR_BASE      // LED for output
+        MOV R1, #0              // counter is set at R1
+L1:     ADD R1, R1, #1          // add 1 to R1
+        STR R1, [R0]            // apply value of R1 to LEDs
+        B   L1                  // loop - branch back to L1
